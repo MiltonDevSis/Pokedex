91ee6ed8a7634346d44b9ea7241a50e1eaf776f6
MiltonDevSis <milton.dev.sis@gmail.com>
2021-10-31 18:29:58 -0300

    Populando recycler com uma lista estatica

--- app/src/main/java/com/mpfcoding/podedex/presentation/MainActivity.kt
+++ app/src/main/java/com/mpfcoding/podedex/presentation/MainActivity.kt
@@ -2,7 +2,11 @@ package com.mpfcoding.podedex.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mpfcoding.podedex.databinding.ActivityMainBinding
+import com.mpfcoding.podedex.domain.Pokemon
+import com.mpfcoding.podedex.domain.PokemonType
+import com.mpfcoding.podedex.presentation.adapter.PokemonAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +17,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val recyclerView = binding.recyclerView
+
+        val pok = Pokemon(
+                "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png",
+                9,
+                "Pikachu",
+                listOf(
+                    PokemonType("Earth"),
+                    PokemonType("Teste")
+                )
+            )
+
+        val pokemons = listOf(pok, pok, pok)
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = PokemonAdapter(pokemons)
     }
 }
